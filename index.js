0eaea1cf7a3b6db21ff528da12a2a7083225e06e
Lucas Karlsson <38735934+KarlssonLucas@users.noreply.github.com>
2021-09-06 01:47:38 +0200

    setting for heroku port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const db = require('./queries')
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
