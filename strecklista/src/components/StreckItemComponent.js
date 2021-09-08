c6784e0e3ee27887dd3f66a574a8bcc8466cb220
Lucas Karlsson <38735934+KarlssonLucas@users.noreply.github.com>
2021-09-08 14:07:24 +0200

    funcionallity to the item component

--- strecklista/src/components/StreckItemComponent.js
+++ strecklista/src/components/StreckItemComponent.js
@@ -4,13 +4,14 @@ import { useHistory } from 'react-router-dom';
 
 const StreckItemComponent = (props) => {
     const [items, setItems] = useState([]);
-    
+    const [amount, setAmount] = useState(1);
+   
     return (
         <div className="item">
                 <div className="streckaItem"> 
-                        <div className="subtract"> - </div>
-                        <input type="text" className="inputfield" defaultValue="1"/>
-                        <div className="addition"> + </div>
+                        <div className="subtract" onClick= {() => amount<2 ? true : setAmount(amount-1)}> - </div>
+                        <input type="text" className="inputfield" readOnly={true} value={amount}/>
+                        <div className="addition" onClick= {() => setAmount(amount+1)}> + </div>
                 </div>
                 <div className="name"> {props.name} </div>
                 <div className="pris"> {props.pris} kr </div>
