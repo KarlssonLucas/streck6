9c0ba4b66b1d7e4c3bab7fcd3466802e54cb4e17
Lucas Karlsson <Lucaskarlsson@live.se>
2021-10-19 23:23:59 +0200

    history components fixed and now materialUI goodies

--- strecklista/src/components/HistoryComponent.js
+++ strecklista/src/components/HistoryComponent.js
@@ -2,12 +2,28 @@ import "../css/historycomponent.css";
 import React, { useState, useEffect } from 'react';
 import Paper from '@mui/material/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import { Divider } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const styles = makeStyles({
     paper: {
         backgroundColor: 'red'
     },
 });
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#ff0000',
+      },
+      secondary: {
+        main: '#45a29e',
+      },
+    },
+  });
 
 const HistoryComponent = (props) => {
     const [hist, setHist] = useState([]);
@@ -45,7 +61,19 @@ const HistoryComponent = (props) => {
         <div>
         {hist.slice(0).reverse().map(i => (
             <div key={i.id} className="">
-                <Paper elevation={6} className="muipaper"/>
+                <Paper elevation={6} className="muipaper">
+                    <Stack spacing={0.5}>
+                        <div className="contentHistory">
+                            <div> {i.name} </div>
+                            <div> {i.streck}{i.name === "Inbetalning" ? "kr" : "st"} </div>
+                            <div> {i.time.substring(0,10)} </div>
+                            <hr className="lineBreakHistory"></hr>
+                            <ThemeProvider theme={theme}>
+                            <Button variant="outlined" startIcon={<DeleteIcon />}>Delete</Button>
+                            </ThemeProvider>
+                        </div>
+                    </Stack>
+                </Paper>
             </div>
             ))}
         </div>
